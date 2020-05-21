9319d009613899d20c0fc128338e8bd4aaba5703
Johnnathan Baxter <jbaxter6@radford.edu>
2020-05-21 00:38:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,8 +4,18 @@ def nyc_pigeon_organizer(data)
   # write your code here
   
   data.each_with_object({}) do |(key, value), hash| 
-    binding.pry
-    hash
+    # binding.pry
+    
+    value.each do |inner_key, names|
+      binding.pry
+      names.each do |name|
+        
+        if !hash[name]
+          hash[name] = {}
+          
+          
 end
-binding.pry
+end
+end
+end
 end
