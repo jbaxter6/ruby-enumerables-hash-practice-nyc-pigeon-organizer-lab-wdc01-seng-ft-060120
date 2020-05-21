e66ef0b97f92b1d04002e6d4112b1bca7cf091d0
Johnnathan Baxter <jbaxter6@radford.edu>
2020-05-21 00:47:53 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # write your code here
+  
+  data.each_with_object({}) do |(key, value), hash| 
+    # binding.pry
+    
+    value.each do |inner_key, names|
+      # binding.pry
+      names.each do |name|
+        # binding.pry
+        if !hash[name]
+          hash[name] = {}
+        end
+        if !hash[name][key]
+          hash[name][key] = []
+        end
+      
+      hash[name][key].push(inner_key.to_s)
+      
+      end
+  
+      end
+
+    end
 end
